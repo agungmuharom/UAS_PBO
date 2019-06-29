4676a77b599ecf6cfdca3648eefec07be417718e
agungmuharom <51038076+agungmuharom@users.noreply.github.com>
2019-06-29 20:08:03 +0700

    UAS PBO-A2.1700008-AGUNG MUHAROM-KARYAWAN

---
+++ superhero.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * NAMA : AGUNG MUHAROM
+ * NIM  : A2.1700008
+ * KELAS: TI IVE J&S KARYAWAN
+ */
+
+public class superhero {
+    String namaSuperhero;
+    
+public superhero (){
+    
+}
+public String getnamaSuperhero(){
+    return namaSuperhero;
+}
+public void setnamaSuperhero(String namaSuperhero){
+    this.namaSuperhero = namaSuperhero;
+}
+}
+
